c8b39766e88bf89ee33b60249d9b1c808201b4f9
Benjamin Herzog <benjamin.herzog@lovoo.com>
2016-02-01 17:48:39 +0100

    Added framework target

---
+++ Demo/ZFModalTransition/ZFModalTransition.h
@@ -0,0 +1,19 @@
+//
+//  ZFModalTransition.h
+//  ZFModalTransition
+//
+//  Created by Benjamin Herzog on 01/02/16.
+//  Copyright © 2016 zoonref. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFModalTransition.
+FOUNDATION_EXPORT double ZFModalTransitionVersionNumber;
+
+//! Project version string for ZFModalTransition.
+FOUNDATION_EXPORT const unsigned char ZFModalTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFModalTransition/PublicHeader.h>
+
+
